8c094f82908658d97ad0da07f2a019086c3dc182
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-07-26 19:55:39 +0530

    updated

--- routes/places.js
+++ routes/places.js
@@ -28,7 +28,7 @@ function haversineDistanceMeters(lat1, lon1, lat2, lon2 ){
     const a = Math.sin(dLat /2)** 2+ Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon/2)**2;
     return R*2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 }
-
+//fetching location according to categories
 function normalizeElement(el, category, originLat, originLon){
     const lat = el.lat ?? el.center ?.lat;
     const lon = el.lon ?? el.center ?.lon;
